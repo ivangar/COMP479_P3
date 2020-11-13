d8289fbd4dcbe20b573fdb8fec06b19d878d48fd
ivan <garzon861@gmail.com>
2020-11-13 00:09:08 -0500

    Modified functionality to count until reaching block_size of 500 terms.

--- spimi.py
+++ spimi.py
@@ -5,29 +5,33 @@ import re
 import string
 import json
 import itertools
+from collections import defaultdict
+
+
+block_size = 0
 
 
 def generate_token_list():
 
     # load reuters files with the help of NLTK's PlaintextCorpusReader
-    sgm_files = PlaintextCorpusReader("reuters", '.*\.sgm')
-    token_list = []
-    unsorted_token_list = open("files/unsorted_token_list.txt", 'w')  # use for pretty print the list
+    sgm_files = PlaintextCorpusReader("test", '.*\.sgm')
+    hash_dict = defaultdict(list)
 
     for fileid in sgm_files.fileids():
-        f = open("reuters" + '/' + fileid)
+        f = open("test" + '/' + fileid)
         sgm_file = f.read()
         parsed_sgm = BeautifulSoup(sgm_file, 'html.parser')
 
         for document_text in parsed_sgm.find_all('reuters'):
-            block_tokenizer(document_text, token_list, unsorted_token_list)
+            spimi_invert(hash_dict, document_text)
 
     with open("files/unsorted_token_list.json", mode="w", encoding="utf-8") as myFile:
-        json.dump(token_list, myFile)
+        json.dump(hash_dict, myFile)
 
 
-def block_tokenizer(document_text, token_list, unsorted_token_list):
+def spimi_invert(hash_dict, document_text):
 
+    global block_size
     doc_id = int(document_text['newid'])
     doc_text = str(document_text.find('text'))
     raw = BeautifulSoup(doc_text, 'html.parser').get_text()
@@ -37,9 +41,13 @@ def block_tokenizer(document_text, token_list, unsorted_token_list):
         raw = raw.replace(c, " ")
     tokens = word_tokenize(raw)
     for token in tokens:
-        token_list.append((token, doc_id))
-        print(json.dumps([token, doc_id]), file=unsorted_token_list)
-
+        if block_size == 500:
+            block_size = 0
+            #sort terms in dict, write block to file BlockX
+            #clean hash_dict, increment file number by 1
+            #Continue parsing tokens
+        block_size += 1
+        hash_dict[token].append(doc_id)
 
 
 #Use this helper function to test a small amount of data
